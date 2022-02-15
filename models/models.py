a543a9d422b59c96a378cc2b5c35f907431a291c
Loan (lse) <lse@odoo.com>
2022-02-15 18:39:53 +0100

    Adapt for social media stuff

--- models/models.py
+++ models/models.py
@@ -4,12 +4,13 @@ from odoo import api, models, fields
 from odoo.tools import dumpstacks
 
 import logging
+import random
 
 _logger = logging.getLogger(__name__)
 
 class TechSupportOverrideModuleClass(models.Model):
-    _name = 'account.bank.statement.line'
-    _inherit = ['account.bank.statement.line']
+    _name = 'social.account'
+    _inherit = ['social.account']
 
     def track(self, vals):
         _logger.info("LSE tracker triggered \nself %s \nvalues %s",
@@ -18,17 +19,21 @@ class TechSupportOverrideModuleClass(models.Model):
         # dumpstacks()
 
     @api.model
-    def _create(self, data_list):
-        _logger.warning("Call to _create")
+    def _write(self, data_list):
+        r = random.randint(0, 100000)
+        _logger.info(f"{r} LSE tracker Call to _write")
+        _logger.info(f"{r} before: {self.read(['id', 'is_media_disconnected'])}")
         self.track(data_list)
-        created = super(TechSupportOverrideModuleClass, self)._create(data_list)
-        _logger.warning(created.read(["id", "name", "partner_name", "payment_ref", "amount"]))
+        created = super(TechSupportOverrideModuleClass, self).create(data_list)
+        _logger.info(f"{r} after: {created.read(['id', 'is_media_disconnected'])}")
         return created
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        _logger.warning("Call to create")
-        self.track(vals_list)
-        created = super(TechSupportOverrideModuleClass, self).create(vals_list)
-        _logger.warning(created.read(["id", "name", "partner_name", "payment_ref", "amount"]))
-        return created
+    # @api.model_create_multi
+    # def write(self, vals_list):
+    #     r = random.randint(100000, 200000)
+    #     _logger.info(f"{r} LSE tracker Call to write")
+    #     _logger.info(f"{r} before: {self.read(['id', 'is_media_disconnected'])}")
+    #     self.track(vals_list)
+    #     created = super(TechSupportOverrideModuleClass, self).create(vals_list)
+    #     _logger.info(f"{r} after: {created.read(['id', 'is_media_disconnected'])}")
+    #     return created
